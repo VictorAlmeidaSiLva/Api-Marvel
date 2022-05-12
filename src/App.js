0cd32afa7e5b9ae07946b0a953ffccd48158a8de
VictorAlmeidaSiLva <victoraugustoas41@gmail.com>
2022-05-12 02:08:18 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import api from "./api";
+import md5 from "md5";
+import "./App.css";
 
 function App() {
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    async function loadComics() {
+      const response = await api.get("/v1/public/comics", {
+        params: {
+          api_key: "e51b6f20392eb98fcfdf61a26a53c75d536762ff",
+        },
+      });
+      setComics(response.data.results);
+    }
+    loadComics();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        {comics.map((comics) => {
+          return
+          <div>{comics.id}</div>
+        })}
+      </center>
     </div>
   );
 }
-
 export default App;
